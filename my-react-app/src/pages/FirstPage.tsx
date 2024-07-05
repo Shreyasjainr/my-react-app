411f9422a8d425763a12de74b649ca714384a7d4
Shreyas Jain R  P <shreyasj135@gmail.com>
2024-07-05 13:50:24 +0530

    comment remove

--- my-react-app/src/pages/FirstPage.tsx
+++ my-react-app/src/pages/FirstPage.tsx
@@ -1,4 +1,4 @@
-// src/pages/FirstPage.tsx
+
 import React, { useState } from 'react';
 import { Button, TextField, Container, Typography } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
